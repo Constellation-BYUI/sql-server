977bcbbcea6d284d749a3d887a6ae1d0a698b398
TeeLusk <44889927+TeeLusk@users.noreply.github.com>
2020-09-01 09:17:13 -0600

    Added shell scripts to delete and recreate log files. Should be able to pass arguments for each of the pods

---
+++ recreate-log.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cluster=${CLOUDSDK_CONTAINER_CLUSTER:-$(gcloud config get-value container/cluster 2> /dev/null)}
+region=${CLOUDSDK_COMPUTE_REGION:-$(gcloud config get-value compute/region 2> /dev/null)}
+zone=${CLOUDSDK_COMPUTE_ZONE:-$(gcloud config get-value compute/zone 2> /dev/null)}
+project=${CLOUDSDK_CORE_PROJECT:-$(gcloud config get-value core/project 2> /dev/null)}
+gcloud container clusters get-credentials --project="$project" --zone="$zone" "$cluster"
+kubectl exec `kubectl get pod -l app=$1 --template "{{ with index .items 0}}{{ .metadata.name}}{{ end }}"` -- mkdir /var/opt/mssql/log
