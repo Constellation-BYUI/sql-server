6b77024c64b4ddb9364fc3e443d96d84912d7d4a
Teelusk <lus18001@byui.edu>
2020-09-09 22:04:09 +0000

    Updated trigger script

--- delete-log.sh
+++ delete-log.sh
@@ -3,5 +3,5 @@ cluster=${CLOUDSDK_CONTAINER_CLUSTER:-$(gcloud config get-value container/cluste
 region=${CLOUDSDK_COMPUTE_REGION:-$(gcloud config get-value compute/region 2> /dev/null)}
 zone=${CLOUDSDK_COMPUTE_ZONE:-$(gcloud config get-value compute/zone 2> /dev/null)}
 project=${CLOUDSDK_CORE_PROJECT:-$(gcloud config get-value core/project 2> /dev/null)}
- cloud container clusters get-credentials --project="$project" --zone="$zone" "$cluster"
+gcloud container clusters get-credentials --project="$project" --zone="$zone" "$cluster"
 kubectl exec `kubectl get pod -l app=$1 --template "{{ with index .items 0}}{{ .metadata.name}}{{ end }}"` -- rm -r /var/opt/mssql/log/
